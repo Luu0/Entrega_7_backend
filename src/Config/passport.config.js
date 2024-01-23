43696910027bac7d637c29962b4fdcc5dd1a6d41
Valentino Lucero <valentinolucero93gmail.com>
2024-01-22 23:10:44 -0300

    subiendo los archivos

---
+++ src/Config/passport.config.js
@@ -0,0 +1,77 @@
+import passport from "passport";
+import passportlocal from "passport-local";
+import UserModel from "../models/user.js";
+import { createHash, isValidPassword} from "../dirname.js";
+
+
+const localStrategy = passportlocal.Strategy
+
+const initializePassport =()=>{
+
+  passport.use("register", new localStrategy(
+    {passReqToCallback: true, usernameField: "email"},
+    
+    async (req, username, password, done) =>{
+      const {first_name, last_name, email,age,password} = req.body;
+      try{
+        const exist = await UserModel.findOne({email});
+        if(exist){
+          console.log("El user ya existe!!");
+          done(null,false)
+        }
+
+        const user={
+          first_name,
+          last_name,
+          email,
+          age,
+          password:createHash(password)
+        }
+        const result = await UserModel.create(user);
+        return done (null, result)
+      }catch(error){
+        return done("Error registrando al usuario " + error);
+      }
+    }
+  ))
+
+
+  passport.use("login", new localStrategy(
+    {passReqToCallback: true, usernameField: "email"},
+
+    async(req, username, password, done)=>{
+      try{
+        const user = await UserModel.findOne({email:username});
+        console.log("Usuario encontrado para login:");
+        console.log(user);
+        if (!user) {
+          console.warn("User doesn't exists with username: " + username);
+          return done(null, false);
+        }
+        if (!isValidPassword(user, password)) {
+            console.warn("Invalid credentials for user: " + username);
+            return done(null, false);
+        }
+        return done(null, user);
+
+      }catch{
+        return done(error);
+      }
+    }
+
+  ));
+
+  //Funciones de Serializacion y Desserializacion
+  passport.serializeUser((user, done) => {
+    done(null, user._id)
+  })
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+        let user = await userModel.findById(id);
+        done(null, user)
+    } catch (error) {
+        console.error("Error deserializando el usuario: " + error);
+    }
+  })
+}
---
+++ src/routes/session.router.js
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import UserModel from "../models/user.js";
+import { createHash, isValidPassword } from "../dirname.js";
+import passport from "passport";
+
+
+const router = Router()
+
+router.post('/register', passport.authenticate("register", {
+  failureRedirect: "api/session/fail-register"
+}), async (req,res)=>{
+
+res.send({status:"success", msg:"User created"})
+})
+
+
+router.post('/login', passport.authenticate("login", {
+  failureRedirect: "api/session/fail-login"
+}), async (req,res) => {
+  // let rol;
+  // if(email == "adminCoder@coder.com" && password == "adminCod3r123"){
+  //     rol = "admin"
+  // }
+  // else{
+  //     rol = "user"
+  // }
+  const user = req.user;
+  const rol = "user"
+
+  req.session.user = {
+      name: `${user.first_name} ${user.last_name}`,
+      email: user.email,
+      age: user.age,
+      rol: rol
+  }
+
+  res.send({status:'success', payload: req.session.user, message:'Log successful'})
+})
+
+router.get('/logout',  (req,res)=>{
+  req.session.destroy(err =>{
+    if(!err) return res.status(200).send("deslogueo exitoso")
+    else res.send("fallo el deslogueo")
+  })
+})
+export default router;
